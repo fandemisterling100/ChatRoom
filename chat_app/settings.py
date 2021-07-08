75782e830dbd5f23f9c9c3a9fad73f9687a2dda4
fandemisterling100 <mi.jaramillo@uniandes.edu.co>
2021-07-08 09:50:37 -0500

    Hide Django secret key

--- chat_app/settings.py
+++ chat_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-he!0=jb#%*904b&dd_8jvuvj!%g-1o2&+4ab0h!h#5vq&y=$5h'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
